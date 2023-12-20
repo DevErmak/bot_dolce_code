6aaa534b49349ca07abf1b061b1dc89fd4a457ad
DevErmak <o.khasanov.dev@gmail.com>
2023-12-20 17:17:54 +0300

    savedata

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const Telegraf = require("telegraf");
+
+const bot = new Telegraf("");
+
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "hello there! Welcome to my new telegram bot.",
+    {}
+  );
 });
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
 
 app.listen(process.env.PORT);
