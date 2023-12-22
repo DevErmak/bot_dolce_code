24ddf1396c2812fdd999152f32ec1770f276520e
oleg-creator-arch <oleghasanov999@gmail.com>
2023-12-22 19:59:28 +0300

    savedata

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+app.use(express.json());
+const TelegramBot = require("node-telegram-bot-api");
 
-const { Telegraf } = require("telegraf");
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+// const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+// bot.launch();
 
-bot.command("start", (ctx) => {
-  console.log(ctx.from);
-  bot.telegram.sendMessage(
-    ctx.chat.id,
-    "hello there! Welcome to my new telegram bot.",
-    {}
-  );
+// bot.command("start", (ctx) => {
+//   console.log(ctx.from);
+//   bot.telegram.sendMessage(
+//     ctx.chat.id,
+//     "hello there! Welcome to my new telegram bot.",
+//     {}
+//   );
+// });
+
+app.post("/sendMessage", function (req, res) {
+  bot.sendMessage(873804475, `<b>company</b>`, { parse_mode: "HTML" });
+
+  // bot.telegram.sendMessage(
+  //   873804475,
+  //   `company:${req.body.company}\nphone:${req.body.phone}\nclient:${req.body.client}\nemail:${req.body.email}\n`
+  // );
+  res.end();
 });
 
-bot.telegram.sendMessage(873804475, "hi123");
-
-bot.launch();
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+// process.once("SIGINT", () => bot.stop("SIGINT"));
+// process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 // app.get("/", function (req, res) {
 //   res.send("Hello World");
